b00c3c5817695c47749061b3aec72b2f8c30f92e
Daniel Kats <dbkats@gmail.com>
2014-08-28 13:03:40 -0700

    style

--- board.py
+++ board.py
@@ -1,19 +1,16 @@
 def sq_to_index(sq):
 	"""Expects sq to be a 2-character string.
 	First character is lower case [a-h], second character is number [1-8]"""
-
-	# goal: A -> 0, H -> 7
 	col = ord(sq[0]) - 97
-	# black is at the top
-	# goal: 1 -> 7, 8 -> 0
 	row = 8 - int(sq[1])
-
 	return (row, col)
+
 
 def index_to_sq(row, col):
 	pt1 = chr(col + 97)
 	pt2 = 8 - row
 	return "%s%d" % (pt1, pt2)
+
 
 def is_valid_square(row, col):
 	return 0 <= row and row < 8 and 0 <= col and col < 8
@@ -21,6 +18,7 @@ def is_valid_square(row, col):
 
 def opposite_color(col):
 	return ("W" if col == "B" else "B")
+
 
 class Board(object):
 	EMPTY = ""

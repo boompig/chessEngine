5ba5f7e265e331ba344e6b3a6c5315e292b97b20
Daniel Kats <boompigdev@gmail.com>
2019-09-12 15:49:59 -0700

    create weird mates folder

--- tests/pgn_test.py
+++ tests/pgn_test.py
@@ -1,20 +1,21 @@
 """
 This file makes sure that full chess games can be played
 """
+import os
 
 import chess.pgn
 from chess_engine import Board
 from chess_engine.board import WHITE, BLACK
 
 
-def read_pgn_moves(fname):
+def read_pgn_moves(fname: str):
     with open(fname) as fp:
         first_game = chess.pgn.read_game(fp)
         return first_game.headers.get("Result"), first_game.mainline_moves()
 
 
-def run_pgn(fname):
-    _, moves = read_pgn_moves(fname)
+def run_pgn(fname: str):
+    result, moves = read_pgn_moves(fname)
     board = Board()
 
     for i, move in enumerate(moves):
@@ -28,7 +29,7 @@ def run_pgn(fname):
         board.move_piece(uci[:2], uci[2:4], promotion=(uci[4].upper() if move.promotion else None))
         board.print()
     # and that's it
-    return board
+    return board, result
 
 
 def test_weird_promotions():
@@ -37,7 +38,7 @@ def test_weird_promotions():
     lots of weird promotions here
     """
     fname = "data/macdonnell_bird_1874.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
@@ -46,9 +47,9 @@ def test_weird_promotions_2():
     Taken from here: http://www.chessgames.com/perl/chessgame?gid=1075778
     lots of weird promotions here
     """
-    fname = "data/wiede_goetz_1880.pgn"
-    board = run_pgn(fname)
-    assert board.is_in_checkmate(WHITE)
+    fname = "data/sandrin_le_cornu_1949.pgn"
+    board, _ = run_pgn(fname)
+    assert board.is_in_checkmate(BLACK)
 
 
 def test_stalemate():
@@ -56,21 +57,21 @@ def test_stalemate():
     Taken from here: http://www.chessgames.com/perl/chessgame?gid=1255706
     """
     fname = "data/stalemate.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert board.is_in_stalemate(WHITE)
 
 
 def test_kasparov_topalov():
     """Taken from here: http://www.chessgames.com/perl/chessgame?gid=1011478"""
     fname = "data/kasparov_topalov_1999.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
 def test_aronian_anand():
     """Taken from here: http://www.chessgames.com/perl/chessgame?gid=1451858"""
     fname = "data/aronian_anand_2007.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
@@ -79,7 +80,7 @@ def test_anand_carlsen_2013_game_5():
     Taken from here: http://www.chessgames.com/perl/chessgame?gid=1737319
     """
     fname = "data/carlsen_anand_2013_game_5.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
@@ -88,7 +89,7 @@ def test_anand_carlsen_2013_game_6():
     Taken from here: http://www.chessgames.com/perl/chessgame?gid=1737460
     """
     fname = "data/anand_carlsen_2013_game_6.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
@@ -97,14 +98,14 @@ def test_anand_carlsen_2013_game_9():
     Taken from here: http://www.chessgames.com/perl/chessgame?gid=1737896
     """
     fname = "data/anand_carlsen_2013_game_9.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
 def test_carlsen_bu_2017():
     """Taken from here: http://www.chessgames.com/perl/chessgame?gid=1884908"""
     fname = "data/carlsen_bu_xiangzhi_2017.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
@@ -112,7 +113,7 @@ def test_carlsen_karjakin_2016_game_16():
     """Taken from: http://www.chessgames.com/perl/chessgame?gid=1848607
     This game is interesting because both players castle and it has en-passant"""
     fname = "data/carlsen_karjakin_2016_game_16.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert (not board.is_in_checkmate(WHITE) and
             not board.is_in_checkmate(BLACK) and
             board.is_in_check(BLACK))
@@ -122,39 +123,39 @@ def test_carlsen_caruana_2018_game_13():
     """Taken from http://www.chessgames.com/perl/chessgame?gid=1937925
     """
     fname = "data/carlsen_caruana_2018.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 
 def test_aronian():
     """from http://www.chessgames.com/perl/chessgame?gid=1399152"""
     fname = "data/aronian_anand_2007.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 def test_mvl():
    """from http://www.chessgames.com/perl/chessgame?gid=1729531"""
    fname = "data/vachier-lagrave_caruana_2013.pgn"
-   board = run_pgn(fname)
+   board, _ = run_pgn(fname)
    assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 def test_alekhine_many_promos():
     """http://www.chessgames.com/perl/chessgame?gid=1282607
     interesting because has many promotions"""
     fname = "data/alekhine_nn_1915.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert board.is_in_checkmate(BLACK)
 
 def test_alekhine_quad_pawns():
     """from http://www.chessgames.com/perl/chessgame?gid=1011704"""
     fname = "data/alekhine_nenarokov_1907.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert not board.is_in_checkmate(WHITE) and not board.is_in_checkmate(BLACK)
 
 def test_fischer_mate():
     """from http://www.chessgames.com/perl/chessgame?gid=1242850"""
     fname = "data/fischer_greenblatt_1977.pgn"
-    board = run_pgn(fname)
+    board, _ = run_pgn(fname)
     assert board.is_in_checkmate(BLACK)
 
 
@@ -163,15 +164,12 @@ def test_weird_mates():
     - http://www.chessgames.com/perl/chessgame?gid=1075778
     - http://www.chessgames.com/perl/chessgame?gid=1284180
     - http://www.chessgames.com/perl/chessgame?gid=1358125
+    - http://www.chessgames.com/perl/chessgame?gid=1259987
     """
-    fnames = [
-        "data/wiede_goetz_1880.pgn",
-        "data/e_clemenz_eisenschmidt_1862.pgn",
-        "data/malinin_savinov_1988.pgn",
-    ]
-    for i, fname in enumerate(fnames):
-        board = run_pgn(fname)
-        if i == 0:
+    for fname in os.listdir("data/weird-mates"):
+        path = os.path.join("data/weird-mates", fname)
+        board, result = run_pgn(path)
+        if result == "0-1":
             assert board.is_in_check(WHITE)
             assert board.is_in_checkmate(WHITE)
         else:
